7530b7d1f796630bdebf5cb8ac7dec5e63cd9196
Jashan190 <singhjashandeep190@gmail.com>
2025-08-04 22:08:51 -0700

    backend start

---
+++ manaakhah-backend/src/main/java/com/manaakhah/backend/ManaakhahBackendApplication.java
@@ -0,0 +1,13 @@
+package com.manaakhah.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManaakhahBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManaakhahBackendApplication.class, args);
+	}
+
+}
---
+++ manaakhah-backend/src/test/java/com/manaakhah/backend/ManaakhahBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manaakhah.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManaakhahBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
